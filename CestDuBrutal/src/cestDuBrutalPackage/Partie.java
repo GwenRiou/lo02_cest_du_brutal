a87ceda74bc90bc86694b2b02fffc4780487f91b
GwenRiou <gwenaelle.riou@utt.fr>
2022-11-15 09:04:59 +0100

    singleton Partie

--- CestDuBrutal/src/cestDuBrutalPackage/Partie.java
+++ CestDuBrutal/src/cestDuBrutalPackage/Partie.java
@@ -1,16 +1,50 @@
 package cestDuBrutalPackage;
 
 public class Partie {
+    private static Partie partieObject;
     private int etape;
     private boolean finDePartie;
     
-    public void Parite(){
+    
+    private void Parite(){ // constructeur en Private car singleton
         this.etape=0;
         this.finDePartie= false;
         
     }
     
+    public static Partie getInstance() { //--> méthode qui va appeler le constructeur si besoin
+        
+        //create objet if it's not already created
+        if(partieObject == null) {
+            partieObject = new Partie();
+        }
+        
+        //returns the singleton
+        return partieObject;
+    }
+    
+    // Regarde si l'objet Partie a été crée
+    public void getConnection() {
+        System.out.println("You have now a Partie going");
+    }
+    
+    //setter & getter
+    public void setEtape(int etape) {
+        this.etape=etape;
+    }
+    
     public int getEtape() {
         return this.etape;
+    }
+    
+    //THE MAIN
+    public static void main(String[] args) {
+        
+        //Création de la partie
+        Partie partie;
+        partie = Partie.getInstance();
+        partie.getConnection();// ne fonctionne que après un getInstance 
+        
+
     }
 }
