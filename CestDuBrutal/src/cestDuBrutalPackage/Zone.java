08ba6dc85505573ab3df635690d9cd636990d5b1
boone <boone@Toaster-on-steroids>
2022-11-19 23:54:45 +0100

    Added Enum Setters and Getters

--- CestDuBrutal/src/cestDuBrutalPackage/Zone.java
+++ CestDuBrutal/src/cestDuBrutalPackage/Zone.java
@@ -3,7 +3,7 @@ import java.util.*;//VERY IMPORTANT
 
 public class Zone {
     private String zoneName;
-    
+    private ControleZone enumControleZone;
     
     public void affectation() {
         
@@ -18,7 +18,9 @@ public class Zone {
     
     //getters
     public String getZoneName() {return zoneName;}
+    public ControleZone getStrategie() {return enumControleZone;}
     //setters
     public void setZoneName(String zoneName) {this.zoneName = zoneName;}
+    public void setStrategie(String enumControleZone) {this.enumControleZone = ControleZone.valueOf(enumControleZone.toUpperCase());}
     
 }
